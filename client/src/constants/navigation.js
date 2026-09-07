53db27c47fbff2a965597d80f198029db01c0d95
Ghost <teammate.28.2024@gmail.com>
2026-09-07 14:59:20 +0530

    Update navigation.js

--- client/src/constants/navigation.js
+++ client/src/constants/navigation.js
@@ -2,7 +2,7 @@ import {
   FiHome, FiUsers, FiClock, FiCalendar, FiBriefcase,
   FiTrello, FiDollarSign, FiBell, FiFolder, FiPieChart,
   FiSettings, FiMessageSquare, FiCreditCard,
-  FiVideo, FiSpeaker, FiUser, FiCheckSquare, FiGlobe, FiServer, FiZap,
+  FiVideo, FiSpeaker, FiUser, FiCheckSquare, FiGlobe, FiZap,
 } from 'react-icons/fi'
 import { ROLES } from './index'
 
@@ -18,8 +18,6 @@ export const NAV_ITEMS = [
   { key: 'clients', label: 'Clients', path: '/clients', icon: FiGlobe,
     roles: [ROLES.ADMIN, ROLES.MANAGER] },
   { key: 'domains', label: 'Domains', path: '/domains', icon: FiGlobe,
-    roles: [ROLES.ADMIN, ROLES.MANAGER] },
-  { key: 'hosting', label: 'Hosting', path: '/hosting', icon: FiServer,
     roles: [ROLES.ADMIN, ROLES.MANAGER] },
   { key: 'projects', label: 'Projects', path: '/projects', icon: FiTrello, roles: A },
   { key: 'attendance', label: 'Attendance', path: '/attendance', icon: FiClock, roles: A },
